795e08c15612fd66975fc79cd8e9ed325d277ee4
vesper1978 <28059535+vesper1978@users.noreply.github.com>
2020-09-25 21:09:57 -0300

    Update config.js

--- overlay/root/rtorrent/flood/config.js
+++ overlay/root/rtorrent/flood/config.js
@@ -1,47 +1,173 @@
+const {spawn} = require('child_process');
+const crypto = require('crypto');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const {argv} = require('yargs')
+  .env('FLOOD_OPTION_')
+  .option('baseuri', {
+    default: '/',
+    describe: "This URI will prefix all of Flood's HTTP requests",
+    type: 'string',
+  })
+  .option('rundir', {
+    alias: 'd',
+    default: path.join(os.homedir(), '.local/share/flood'),
+    describe: "Where to store Flood's runtime files (eg. database)",
+    type: 'string',
+  })
+  .option('host', {
+    alias: 'h',
+    default: '127.0.0.1',
+    describe: 'The host that Flood should listen for web connections on',
+    type: 'string',
+  })
+  .option('port', {
+    alias: 'p',
+    default: 3000,
+    describe: 'The port that Flood should listen for web connections on',
+    type: 'number',
+  })
+  .option('secret', {
+    alias: 's',
+    describe: 'A unique secret, a random one will be generated if not provided',
+    type: 'string',
+  })
+  .option('noauth', {
+    alias: 'n',
+    default: false,
+    describe: "Disable Flood's builtin access control system, needs rthost+rtport OR rtsocket.",
+    type: 'boolean',
+  })
+  .option('rthost', {
+    describe: "Depends on noauth: Host of rTorrent's SCGI interface",
+    type: 'string',
+  })
+  .option('rtport', {
+    describe: "Depends on noauth: Port of rTorrent's SCGI interface",
+    type: 'number',
+  })
+  .option('rtsocket', {
+    conflicts: ['rthost', 'rtport'],
+    describe: "Depends on noauth: Path to rTorrent's SCGI unix socket",
+    type: 'string',
+  })
+  .option('ssl', {
+    default: false,
+    describe: 'Enable SSL, key.pem and fullchain.pem needed in runtime directory',
+    type: 'boolean',
+  })
+  .option('sslkey', {
+    describe: 'Depends on ssl: Absolute path to private key for SSL',
+    implies: 'ssl',
+    hidden: true,
+    type: 'string',
+  })
+  .option('sslcert', {
+    describe: 'Depends on ssl: Absolute path to fullchain cert for SSL',
+    implies: 'ssl',
+    hidden: true,
+    type: 'string',
+  })
+  .option('allowedpath', {
+    describe: 'Allowed path for file operations, can be called multiple times',
+    type: 'string',
+  })
+  .option('dbclean', {
+    default: 1000 * 60 * 60,
+    describe: 'ADVANCED: Interval between database purge',
+    hidden: true,
+    type: 'number',
+  })
+  .option('maxhistorystates', {
+    default: 30,
+    describe: 'ADVANCED: Number of records of torrent download and upload speeds',
+    hidden: true,
+    type: 'number',
+  })
+  .option('clientpoll', {
+    default: 1000 * 2,
+    describe: 'ADVANCED: How often (in ms) Flood will request the torrent list',
+    hidden: true,
+    type: 'number',
+  })
+  .option('rtorrent', {
+    default: false,
+    describe: 'ADVANCED: rTorrent daemon managed by Flood',
+    hidden: true,
+    type: 'boolean',
+  })
+  .option('proxy', {
+    default: 'http://127.0.0.1:3000',
+    describe: 'DEV ONLY: See the "Local Development" section of README.md',
+    hidden: true,
+    type: 'string',
+  })
+  .help();
+
+if (argv.rtorrent) {
+  const rTorrentProcess = spawn('rtorrent', ['-o', 'system.daemon.set=true']);
+  process.on('exit', () => {
+    console.log('Killing rTorrent daemon...');
+    rTorrentProcess.kill('SIGTERM');
+  });
+}
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+try {
+  fs.mkdirSync(path.join(argv.rundir, 'db'), {recursive: true});
+  fs.mkdirSync(path.join(argv.rundir, 'temp'), {recursive: true});
+} catch (error) {
+  console.error('Failed to access runtime directory');
+  process.exit(1);
+}
+
+const DEFAULT_SECRET_PATH = path.join(argv.rundir, 'flood.secret');
+let secret;
+
+if (!argv.secret) {
+  try {
+    if (fs.existsSync(DEFAULT_SECRET_PATH)) {
+      secret = fs.readFileSync(DEFAULT_SECRET_PATH, {encoding: 'utf8'});
+    } else {
+      const buf = Buffer.alloc(36);
+      crypto.randomFillSync(buf);
+      secret = buf.toString('hex');
+      fs.writeFileSync(DEFAULT_SECRET_PATH, secret, {mode: 0o600});
+    }
+  } catch (error) {
+    console.error('Failed to read or generate secret');
+    process.exit(1);
+  }
+} else {
+  ({secret} = argv);
+}
+
 const CONFIG = {
-  // This URI will prefix all of Flood's HTTP requests. You _must_ have a web
-  // server, like nginx, configured to forward these requests to the Flood
-  // web server.
-  // For example, if you intend to serve from http://example.com/flood, set this to
-  // '/flood' and configure your web server to pass _all_ requests from `/flood` to
-  // the root of Flood's web server.
-  // Recompiling assets with `npm run build` is needed after each `baseURI` change.
-  // See https://github.com/Flood-UI/flood/wiki/Using-Flood-behind-a-reverse-proxy
   baseURI: '/',
-  // Flood uses a local nedb database to keep track of users, torrents,
-  // and activity. The database is regularly purged to remove outdated data.
-  // This value dictates how old data is, in milliseconds, before being purged.
   dbCleanInterval: 1000 * 60 * 60,
-  // Where to store the local nedb database.
   dbPath: './server/db/',
-  // The host that Flood should listen for web connections on.
-  // If you want to connect to Flood from hosts other that the one it is running
-  // on, you should change this value.
-  // To listen on all interfaces, change to `floodServerHost: '0.0.0.0'`..
-  floodServerHost: '0.0.0.0',
-  // The port that Flood should listen for web connections on.
-  floodServerPort: 80,
-  // Used for development. See the "Local Development" section of README.md
-  // for detail.
-  floodServerProxy: 'http://127.0.0.1:80',
-  // Flood keeps a history of torrent download and upload speeds.
-  // This value dictates the number of individual records per period to keep.
-  maxHistoryStates: 30,
-  // How often (in milliseconds) Flood will request the torrent list from.
-  torrentClientPollInterval: 1000 * 2,
-  // A unique secret for signing messages with JWT (see https://jwt.io). Change
-  // this to something unique and hard to guess.
-  secret: 'flood',
-  scgi: {
-    host: '127.0.0.1',
-    port: 5000,
+  tempPath: path.resolve(path.join(argv.rundir, 'temp')),
+  disableUsersAndAuth: argv.noauth,
+  configUser: {
+    host: argv.rthost || 'localhost',
+    port: argv.rtport || 5000,
     socket: true,
-    socketPath: '/home/rtorrent/session/rtorrent.sock'
+    socketPath: '/home/rtorrent/session/rtorrent.sock',
   },
-  // Configuration for SSL, if using SSL with the Flood service directly.
-  ssl: false,
-  sslKey: '/absolute/path/to/key/',
-  sslCert: '/absolute/path/to/certificate/',
+  floodServerHost: '0.0.0.0',
+  floodServerPort: '80',
+  floodServerProxy: 'http://127.0.0.1:80',
+  maxHistoryStates: 30,
+  torrentClientPollInterval: 1000 * 2,
+  secret,
+  ssl: argv.ssl,
+  sslKey: argv.sslkey || path.resolve(path.join(argv.rundir, 'key.pem')),
+  sslCert: argv.sslcert || path.resolve(path.join(argv.rundir, 'fullchain.pem')),
+  allowedPaths: argv.allowedpath ? [].concat(argv.allowedpath) : null,
 };
-// Do not remove the below line.
+
 module.exports = CONFIG;
